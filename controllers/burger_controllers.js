56fe18538aaf8b7bdde73a7df276322f94ea3e65
David Lawrence <dalaw85@gmail.com>
2019-04-30 10:11:03 -0400

    burger add complete

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -16,18 +16,18 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burger", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   burger.create([
-    "burger_name", "devoured"
+    "burger_name"//,"devoured"
   ], [
-    req.body.burger_name, req.body.devoured
+    req.body.burger_name//, req.body.devoured
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burger/:id", function(req, res) {//update to devoured = true
+router.put("/api/burgers/:id", function(req, res) {//update to devoured = true
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -44,7 +44,7 @@ router.put("/api/burger/:id", function(req, res) {//update to devoured = true
   });
 });
 
-router.delete("/api/burger/:id", function(req, res) {
+router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   burger.delete(condition, function(result) {
--- public/assets/js/burgerjs.js
+++ public/assets/js/burgerjs.js
@@ -1,20 +1,45 @@
+
+$(function(){
 $(".create-form").on("submit", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newBurger = {
-      name: $("#burger").val().trim(),
+      burger_name: $("#burger").val().trim(),
       
     };
 
     // Send the POST request.
-    $.ajax("/api/burger", {
+    $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
     );
+});
+
+    $("#eat").on("click", function(event) {
+      var id = $(this).data("id");
+      var eaten = $(this).data("eaten");
+  
+      var newStatus = {
+        devoured: true
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burger/" + id, {
+        type: "PUT",
+        data: newStatus
+      }).then(
+        function() {
+          console.log("burger", newStatus);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+});

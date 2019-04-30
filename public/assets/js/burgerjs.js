96404989ee9550bafa51e83fa6b5cbda01af0d47
David Lawrence <dalaw85@gmail.com>
2019-04-30 11:09:25 -0400

    update works as intended

--- public/assets/js/burgerjs.js
+++ public/assets/js/burgerjs.js
@@ -1,31 +1,29 @@
 
 $(function(){
 $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     var newBurger = {
+
       burger_name: $("#burger").val().trim(),
       
     };
 
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
+        console.log("created new burger" );
         location.reload();
       }
     );
 });
 
-    $("#eat").on("click", function(event) {
+    $(".eat").on("click", function(event) {
       var id = $(this).data("id");
-    //   var eaten = $(this).data("eaten");
-  
+    //   var eaten = $(this).data("eaten");data="{{id}}"
+      
       var newStatus = {
         devoured: true
       };
@@ -37,7 +35,6 @@ $(".create-form").on("submit", function(event) {
       }).then(
         function() {
           console.log("burger", newStatus);
-          // Reload the page to get the updated list
           location.reload();
         }
       );
